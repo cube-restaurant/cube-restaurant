398200db2a631fbf19b597026ab7fd9bf81ad2c8
Coding With Fun <admin@codingwith.fun>
2020-10-24 15:58:39 -0500

    updated backend with apollo server with express

---
+++ backend/src/constants.ts
@@ -0,0 +1,1 @@
+export const __PORT__ = process.env.PORT ? Number(process.env.PORT) : 5000;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,38 @@
-import app from './app';
+/* eslint-disable import/first */
+import 'reflect-metadata';
 
-app.listen(5000, () => {
-  console.log('Suecessfully started express app on http://localhost:5000');
-});
+import dotenv from 'dotenv';
+
+const dotenvResult = dotenv.config();
+if (dotenvResult.error) {
+  throw new Error(dotenvResult.error.message);
+}
+
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
+import { buildSchema } from 'type-graphql';
+
+import { HelloResolver } from './resolvers/hello.resolvers';
+import { __PORT__ } from './constants';
+
+const main = async () => {
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({
+    app,
+    cors: false,
+  });
+
+  app.listen(__PORT__, () => {
+    console.log(`Suecessfully started express app on http://localhost:${__PORT__}`);
+  });
+};
+
+main();
